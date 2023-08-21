c28798c8b261c49310568e3ae94b8634833da6ed
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-08-21 17:26:57 +0530

    changes

--- src/Components/DataGrid.tsx
+++ src/Components/DataGrid.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { DataGrid } from "@mui/x-data-grid";
 import axios from "axios";
 import { IDataGrid } from "../@types";
