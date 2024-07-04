3b738cf6028de07c5c4d14dc394a067268dc2d1b
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-07-04 23:51:40 +0530

    fixing bug in index file

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,6 +1,4 @@
 import DataGrid from "./DataGrid";
-import Department1 from "./Department1";
-import Department2 from "./Department2";
 import Logo from "./Logo";
 import Navbar from "./Navbar";
-export { Department1, Department2, DataGrid, Logo, Navbar };
+export { DataGrid, Logo, Navbar };
